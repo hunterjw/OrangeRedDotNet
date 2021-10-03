6170db1cb0ce1d63296e9dba4d4fe1abf3624fa4
John Hunter <hunterjw.usa@gmail.com>
2021-10-03 18:08:03 -0500

    Initializing projects

---
+++ RedditDotNet.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedditDotNet.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ RedditDotNet.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RedditDotNet.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
