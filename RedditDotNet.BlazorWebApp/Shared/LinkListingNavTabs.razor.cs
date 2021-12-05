c44e10da4e0c60a8ce64bb7275844e9367cae49a
John Hunter <hunterjw.usa@gmail.com>
2021-12-04 18:56:47 -0600

    Adding LinkListingNavTabs component. Removing Home from NavMenu.

---
+++ RedditDotNet.BlazorWebApp/Shared/LinkListingNavTabs.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RedditDotNet.BlazorWebApp.Shared
+{
+    public enum LinkListingTabs
+    {
+        Best,
+        Hot,
+        New,
+        Rising,
+        Controversial,
+        Top
+    }
+
+    public partial class LinkListingNavTabs
+    {
+        [Parameter]
+        public LinkListingTabs ActiveTab { get; set; }
+    }
+}
