41b8434868275e83a9d8ae29a25bd17edc07a652
John Hunter <hunterjw.usa@gmail.com>
2022-01-22 19:07:55 -0600

    Adding basic user profile page

---
+++ RedditDotNet.BlazorWebApp/Pages/Users.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace RedditDotNet.BlazorWebApp.Pages
+{
+    /// <summary>
+    /// Page for displaying user profiles
+    /// </summary>
+    public partial class Users
+    {
+        /// <summary>
+        /// Reddit service
+        /// </summary>
+        [Inject]
+        public Reddit Reddit { get; set; }
+
+        /// <summary>
+        /// Identity service
+        /// </summary>
+        [Inject]
+        public IdentityService IdentityService { get; set; }
+
+        /// <summary>
+        /// Account username
+        /// </summary>
+        [Parameter]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Account 
+        /// </summary>
+        protected Models.Account.Account Account { get; set; }
+
+        /// <inheritdoc/>
+        protected override async Task OnParametersSetAsync()
+        {
+            Account = await Reddit.Users.GetAbout(UserName);
+        }
+    }
+}
---
+++ RedditDotNet.BlazorWebApp/Shared/Users/UserCard.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RedditDotNet.BlazorWebApp.Shared.Users
+{
+    /// <summary>
+    /// User profile card component
+    /// </summary>
+    public partial class UserCard
+    {
+        /// <summary>
+        /// Account data
+        /// </summary>
+        [Parameter]
+        public Models.Account.AccountData AccountData { get; set; }
+
+        /// <summary>
+        /// User title
+        /// </summary>
+        protected string Title { get; set; } = null;
+
+        /// <summary>
+        /// User description
+        /// </summary>
+        protected string Description { get; set; } = null;
+
+        /// <inheritdoc/>
+        protected override void OnParametersSet()
+        {
+            // Title
+            if (!string.IsNullOrWhiteSpace(AccountData?.Subreddit?.Title))
+            {
+                Title = AccountData.Subreddit.Title;
+            }
+            else
+            {
+                Title = AccountData.Name;
+            }
+            // Description
+            if (!string.IsNullOrWhiteSpace(AccountData?.Subreddit?.PublicDescription))
+            {
+                Description = AccountData.Subreddit.PublicDescription;
+            }
+        }
+    }
+}
