8f9f68552b9dd8dcf1291ea531ab1de97bb720df
John Hunter <hunterjw.usa@gmail.com>
2022-08-27 16:33:51 -0500

    Adding tests base class. Adding account tests.

---
+++ OrangeRedDotNet.Tests/RedditEndpoints/AccountTests.cs
@@ -0,0 +1,69 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace OrangeRedDotNet.Tests.RedditEndpoints
+{
+    [TestClass]
+    public class AccountTests : TestBase
+    {
+        [TestMethod]
+        public async Task GetIdentity()
+        {
+            var result = await GetRedditClient().Account.GetIdentity();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task GetKarmaBreakdown()
+        {
+            var result = await GetRedditClient().Account.GetKarmaBreakdown();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task GetPreferences()
+        {
+            var result = await GetRedditClient().Account.GetPreferences();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task SetPreferences()
+        {
+            var client = GetRedditClient();
+            // Grab current preferences, send them back
+            var result = await client.Account.SetPreferences(
+                await client.Account.GetPreferences()
+                );
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task GetTrophies()
+        {
+            var result = await GetRedditClient().Account.GetTrophies();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task GetFriends()
+        {
+            var result = await GetRedditClient().Account.GetFriends();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task GetBlocked()
+        {
+            var result = await GetRedditClient().Account.GetBlocked();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task GetTrusted()
+        {
+            var result = await GetRedditClient().Account.GetTrusted();
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ OrangeRedDotNet.Tests/TestBase.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using OrangeRedDotNet.Authentication;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace OrangeRedDotNet.Tests
+{
+    /// <summary>
+    /// Base class for tests
+    /// </summary>
+    public class TestBase
+    {
+        /// <summary>
+        /// Get the path to the cache file
+        /// </summary>
+        /// <returns>File path</returns>
+        private static string GetCacheFilePath()
+        {
+            return Path.Combine(AppContext.BaseDirectory, $"AuthCache");
+        }
+
+        /// <summary>
+        /// Load a cached TokenResponse
+        /// </summary>
+        /// <returns>Cached TokenResponse</returns>
+        private static TokenResponse Load()
+        {
+            string filePath = GetCacheFilePath();
+            if (File.Exists(filePath))
+            {
+                return JsonConvert.DeserializeObject<TokenResponse>(
+                    File.ReadAllText(filePath));
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Save a TokenResponse
+        /// </summary>
+        /// <param name="value">Value to save</param>
+        private static void Save(TokenResponse value)
+        {
+            string filePath = GetCacheFilePath();
+            string content = JsonConvert.SerializeObject(value);
+            File.WriteAllText(filePath, content);
+        }
+        /// <summary>
+        /// Password authentication options
+        /// </summary>
+        protected static PasswordAuthenticationOptions PasswordAuthenticationOptions { get; } = new();
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        static TestBase()
+        {
+            var configRoot = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddUserSecrets<TestBase>()
+                .Build();
+
+            configRoot.GetSection(nameof(PasswordAuthenticationOptions))
+                .Bind(PasswordAuthenticationOptions);
+        }
+
+        /// <summary>
+        /// Get reddit authentication
+        /// </summary>
+        /// <returns>Reddit authentication</returns>
+        private static IRedditAuthentication GetAuthentication()
+        {
+            return new PasswordAuthentication(
+                    PasswordAuthenticationOptions,
+                    Load, Save);
+        }
+
+        /// <summary>
+        /// Get a reddit client
+        /// </summary>
+        /// <returns>A reddit client</returns>
+        protected static Reddit GetRedditClient()
+        {
+            return new Reddit(
+                GetAuthentication(),
+                new RedditUserAgent
+                {
+                    Name = Assembly.GetEntryAssembly().GetName().Name,
+                    Version = Assembly.GetEntryAssembly().GetName().Version.ToString(3)
+                });
+        }
+    }
+}
