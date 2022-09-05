2162fafe624edeb40b467fbe8d59db354e63383c
John Hunter <hunterjw.usa@gmail.com>
2022-09-05 16:25:24 -0500

    Adding Search tests

---
+++ OrangeRedDotNet.Tests/RedditEndpoints/SearchTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrangeRedDotNet.Models.Parameters;
+using OrangeRedDotNet.Models.Search;
+using System.Threading.Tasks;
+
+namespace OrangeRedDotNet.Tests.RedditEndpoints
+{
+    [TestClass]
+    public class SearchTests : TestBase
+    {
+        [TestMethod]
+        public async Task Search()
+        {
+            SearchResults result = await GetRedditClient().Search.Search(new SearchListingParameters
+            {
+                Query = "test"
+            });
+            Assert.IsNotNull(result);
+        }
+    }
+}
